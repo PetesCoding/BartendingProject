b48ecb2a96dc7adf47750be06ffd905e5fbf72b7
Peter Toso <petertoso@Peters-Air.lan>
2023-12-22 21:49:07 -0600

    Startup web app

--- CocktailWebApp/app.py
+++ CocktailWebApp/app.py
@@ -12,5 +12,30 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/bar")
+def bar():
+    return render_template("bar.html")
+
+
+@app.route("/ingredients")
+def ingredients():
+    return render_template("ingredients.html")
+
+
+@app.route("/cocktails")
+def cocktails():
+    return render_template("cocktails.html")
+
+
+@app.route("/popular")
+def popular():
+    return render_template("popular.html")
+
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=3000, debug=True)
